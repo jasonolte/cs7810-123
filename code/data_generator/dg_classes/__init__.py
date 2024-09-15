66de744b291e9274995d86c179188b8fd80ac966
jasonolte <137525828+jasonolte@users.noreply.github.com>
2024-09-14 22:51:00 -0400

    Add files via upload

---
+++ code/data_generator/dg_classes/__init__.py
@@ -0,0 +1,6 @@
+### CS7810 Intro to Knowledge Engineering
+### Fall 2024
+### Project data generator 
+### Jason H. Nolte, nolte14@wright.edu
+### https://github.com/jasonolte/cs7810-123
+### package init file
---
+++ code/data_generator/dg_classes/dg_drug_class.py
@@ -0,0 +1,5 @@
+### CS7810 Intro to Knowledge Engineering
+### Fall 2024
+### Drug Class for project data generator
+### Jason H. Nolte, nolte14@wright.edu
+### https://github.com/jasonolte/cs7810-123
